b8b3e38e9dfa50e46aad06b8227f2619bf50d6cc
Jayesh Parekh <jparekh75@hotmail.com>
2020-08-15 15:20:43 +0100

    Added Cache retention expiry config

--- MasterOfMalt.Api/Startup.cs
+++ MasterOfMalt.Api/Startup.cs
@@ -1,20 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using MasterOfMalt.Api.Services;
 using MasterOfMalt.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Primitives;
 using Microsoft.Net.Http.Headers;
+using System;
 
 namespace MasterOfMalt.Api
 {
@@ -56,7 +49,7 @@ namespace MasterOfMalt.Api
                 context.Response.GetTypedHeaders().CacheControl = new CacheControlHeaderValue
                 {
                     Public = true,
-                    MaxAge = TimeSpan.FromHours(6)
+                    MaxAge = TimeSpan.FromHours(Configuration.GetValue<int>("CacheRetentionAgeInHours"))
                 };
 
                 context.Response.Headers[HeaderNames.Vary] = new[] {"Accept-Encoding"};
